0c9bebb08ef312507b13a3922d15f69394b337c9
kimberly <kimberlyrojasm@outlook.com>
2022-01-07 19:58:18 -0800

    Inital animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+#Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 1/7/2021
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
